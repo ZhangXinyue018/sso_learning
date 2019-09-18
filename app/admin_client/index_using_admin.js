7c66d6e86c0aa70240026da174cf0393290b3cfe
zhangxinyue <Xinyue.Zhang@ibm.com>
2019-09-18 15:00:53 +0800

    test get user using admin

---
+++ app/admin_client/index_using_admin.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var KeycloakAdminClient = require('keycloak-admin')["default"];
+
+const kcAdminClient = new KeycloakAdminClient({
+    baseUrl: 'http://localhost:8080/auth',
+    realmName: 'demo',
+});
+
+(async () => {
+    try {
+        await kcAdminClient.auth({
+            grantType: 'client_credentials',
+            clientId: 'emailcli',
+            clientSecret: 'f2d2c16a-f74f-4c01-8e87-3a1471ca6005'
+        });
+        // const realm = await kcAdminClient.realms.findOne('demo');
+        // console.log(realm);
+        const users = await kcAdminClient.users.find();
+        console.log(users);
+        // const groups = await kcAdminClient.groups.find();
+        // console.log(groups);
+    } catch (err) {
+        console.log(err);
+    }
+})();
